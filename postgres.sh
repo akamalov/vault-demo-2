01b7cc6e6a9ccde88a5af51ab5a5f3ab3df08e54
andy <andy.davies@reaktor.com>
2020-01-06 15:33:21 +0200

    scripts: move policy creation to postgres setup

--- postgres.sh
+++ postgres.sh
@@ -1,42 +1,29 @@
-#! /bin/bash
+#!/bin/bash -eux
 
 # enable database secrets engine
 echo "vault secrets enable database"
-vault secrets enable database
+vault secrets enable database || echo "database secrets engine already enabled"
+
+read -n 1 -s -r
+
+# create a policy allowing reads from the database secret engine
+echo 'path "database/creds/*" {
+  capabilities = ["read"]
+}' | vault policy write postgres_connector -
 
 read -n 1 -s -r
 
 # configure how to communicate with postgres
-
-echo '
 vault write database/config/postgres \
   plugin_name="postgresql-database-plugin" \
   allowed_roles="*" \
-  connection_url="postgresql://{{username}}:{{password}}@postgres/postgres?sslmode=disable" \
-  username="postgres" \
-  password="postgres"
-'
-vault write database/config/postgres \
-  plugin_name="postgresql-database-plugin" \
-  allowed_roles="*" \
-  connection_url="postgresql://{{username}}:{{password}}@postgres/postgres?sslmode=disable" \
+  connection_url="postgresql://{{username}}:{{password}}@postgres.service.consul/postgres?sslmode=disable" \
   username="postgres" \
   password="postgres"
 
 read -n 1 -s -r
 
 # create the "reader" dbrole: attached to postgres, and can do CRUD operations on tables
-
-echo '
-vault write database/roles/reader \
-  db_name="postgres" \
-  creation_statements=" \
-    CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; \
-    GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" \
-  default_ttl="10m" \
-  max_ttl="1h"
-'
-
 vault write database/roles/reader \
   db_name="postgres" \
   creation_statements=" \
@@ -48,17 +35,6 @@ vault write database/roles/reader \
 read -n 1 -s -r
 
 # create the "writer" dbrole: attached to postgres, and can do CRUD operations on tables
-
-echo '
-vault write database/roles/writer \
-  db_name="postgres" \
-  creation_statements=" \
-    CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; \
-    GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" \
-  default_ttl="10m" \
-  max_ttl="1h"
-'
-
 vault write database/roles/writer \
   db_name="postgres" \
   creation_statements=" \
